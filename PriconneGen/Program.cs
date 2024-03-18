3bfefa06e137b576e0724ffab49644d38dbb24a7
Kento Ooiwa <lunatic.heros@gmail.com>
2024-03-18 18:36:10 +0900

    first commit.

---
+++ PriconneGen/Program.cs
@@ -0,0 +1,16 @@
+﻿var baseUrl = "https://tabbotdev-priconne.netlify.app/";
+
+var dir = new DirectoryInfo(args.FirstOrDefault());
+
+var files = dir.GetFiles("*",SearchOption.TopDirectoryOnly);
+
+var list = new List<string>();  
+
+foreach(var (file,index) in files.Select((e, i) => (e, i)))
+{
+    list.Add($"priconne,{Path.GetFileNameWithoutExtension(file.Name)},{baseUrl}{index:000}.jpg");
+    var movedPath = Path.Combine(file.DirectoryName, $"{index:000}.jpg");
+    file.MoveTo(movedPath);
+}
+
+File.WriteAllLines("list.csv", list);
